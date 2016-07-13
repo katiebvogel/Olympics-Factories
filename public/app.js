1de5d564ee67883b944df7df31441b8b34e77d79
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-13 10:51:59 -0500

    Basic working version, no added styling

--- public/app.js
+++ public/app.js
@@ -1,5 +1,65 @@
-// $( document ).ready( function(){
-//
-//   $('#leftGif').append('http://media0.giphy.com/media/mPIA4KZVXv0ty/giphy.gif');
-//
-// });http://media0.giphy.com/media/mPIA4KZVXv0ty/giphy.gif
+angular.module('olympicPicker', ['ngRoute']);
+
+  angular.module('olympicPicker').config(function($routeProvider, $locationProvider){
+
+  $routeProvider
+    .when('/badminton', {
+      templateUrl: 'views/badminton.html',
+      controller: 'BadController'
+    })
+    .when('/womensgymnastics', {
+      templateUrl: 'views/womgym.html',
+      controller: 'GymController'
+    })
+    .when('/tabletennis', {
+      templateUrl: 'views/tabten.html',
+      controller: 'TabTenController'
+    })
+    .when('/taekwondo', {
+      templateUrl: 'views/taekwondo.html',
+      controller: 'TaeKwonDoController'
+    })
+    .when('/syncswim', {
+      templateUrl: 'views/syncswim.html',
+      controller: 'SyncSwimController'
+    });
+
+    $locationProvider.html5Mode(true);
+})
+
+
+
+angular.module('olympicPicker').controller('BadController', function($scope, $http){
+  $http.get('/olympics/badminton')
+  .then(function(response){
+  $scope.badmintonner = response.data.Name + ' from ' + response.data.Origin;
+  });
+});
+
+angular.module('olympicPicker').controller('GymController', function($scope, $http){
+  $http.get('/olympics/womensgymnastics')
+  .then(function(response){
+  $scope.gymnast = response.data.Name + ' from ' + response.data.Origin;
+  });
+});
+
+angular.module('olympicPicker').controller('TabTenController', function($scope, $http){
+  $http.get('/olympics/tabletennis')
+  .then(function(response){
+  $scope.tableTennisian = response.data.Name + ' from ' + response.data.Origin;
+  });
+});
+
+angular.module('olympicPicker').controller('TaeKwonDoController', function($scope, $http){
+  $http.get('/olympics/taekwondo')
+  .then(function(response){
+  $scope.taeKwonDoer = response.data.Name + ' from ' + response.data.Origin;
+  });
+});
+
+angular.module('olympicPicker').controller('SyncSwimController', function($scope, $http){
+  $http.get('/olympics/syncswimming')
+  .then(function(response){
+  $scope.syncSwimmer = response.data.Name + ' from ' + response.data.Origin;
+  });
+});
